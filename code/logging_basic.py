a8584c130eea719eeb18247073cbe92b40b6bed2
pyneng <pyneng.course@gmail.com>
2018-02-17 15:49:16 +0000

    Try gitpitch Code Delimiter Slides

---
+++ code/logging_basic.py
@@ -0,0 +1,7 @@
+import logging
+
+logging.basicConfig(filename='mylog.log', level=logging.DEBUG)
+
+logging.debug('Сообщение уровня debug')
+logging.info('Сообщение уровня info')
+logging.warning('Сообщение уровня warning')
